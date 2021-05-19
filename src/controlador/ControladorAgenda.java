6402305cb153719d111114a9f6dd31ad86a14f97
iandreumdam1 <iandreumdam1@gmail.com>
2021-05-19 15:34:48 +0200

    Subido lo que no se subió ayer

---
+++ src/controlador/ControladorAgenda.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+
+public class ControladorAgenda {
+	private AgendaContactos agenda; // el modelo;
+
+	public ControladorAgenda() {
+		this.agenda = new AgendaContactos();
+	}
+
+	@FXML
+	public void initialize() {
+
+	}
+
+}
